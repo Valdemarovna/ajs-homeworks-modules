1159a804e20eba7132c7715bc520f62b000d5130
Valdemarovna <147051721+Valdemarovna@users.noreply.github.com>
2025-05-07 11:44:32 +0300

    Add files via upload

---
+++ src/js/domain.js
@@ -0,0 +1,5 @@
+class Character {
+    // content
+}
+
+export default Character;
---
+++ src/js/game.js
@@ -0,0 +1,18 @@
+import Character from './domain.js';
+
+class Game {
+    start() {
+      console.log(Character);
+    }
+  }
+  
+  export class GameSavingData {
+  }
+  
+  export function readGameSaving() {
+  }
+  
+  export function writeGameSaving() {
+  }
+
+  export default Game;
